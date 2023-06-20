476e04b634de0969d83fbc06564fa4f262f008d8
Adam Palhazi <palhaziadev@gmail.com>
2023-06-20 22:12:16 +0200

    added config changes

---
+++ lint-staged.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // Type check TypeScript files
+  '**/*.(ts|tsx)': () => 'node ./node_modules/typescript/bin/tsc --noEmit',
+
+  // Lint then format TypeScript and JavaScript files
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `yarn eslint --fix ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames.join(' ')}`,
+  ],
+
+  // Format MarkDown and JSON
+  '**/*.(md|json)': (filenames) =>
+    `yarn prettier --write ${filenames.join(' ')}`,
+};
